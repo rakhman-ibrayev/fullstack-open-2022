5823134777a83810e73b44e50ce030677eb95c1c
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-06-12 01:41:01 +0600

    finished the exercises for part3a

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,26 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </>
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
+  
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={total} />
+    </div>
+  )
+}
+
+export default Course
