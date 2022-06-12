43f4f610103be40a9dda78b2eeeb3f54a19743b6
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-06-12 20:40:33 +0600

    finished the phonebook exercises for part2c

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Header = ({ header }) => <h1>{header}</h1>
 
@@ -40,15 +41,18 @@ const Filter = ({ handleSearch, searchResults}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchResults, setSearchResults] = useState([...persons])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   
   const addName = (event) => {
     event.preventDefault()
